afa3c519c81e7c508afa8ec14cc51bda292449e7
Elyorbek Xakimov <elyormuhandis@gmail.com>
2023-06-27 16:59:27 +0500

    create eureka service registry and api gateway

--- employee-service/src/main/java/uz/muhandis/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/uz/muhandis/employeeservice/service/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uz.muhandis.employeeservice.dto.DepartmentDto;
 
-@FeignClient(url = "http://localhost:8080/", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
--- service-registry/src/main/java/uz/muhandis/serviceregistery/ServiceRegistryApplication.java
+++ service-registry/src/main/java/uz/muhandis/serviceregistery/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package uz.muhandis.serviceregistery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
