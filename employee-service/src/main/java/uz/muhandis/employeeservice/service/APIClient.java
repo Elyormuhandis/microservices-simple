9db0e175e557f3b2c39821a6f0be6dbaaa72ba9c
Elyorbek Xakimov <elyormuhandis@gmail.com>
2023-07-14 18:38:11 +0500

    bugs fix

--- employee-service/src/main/java/uz/muhandis/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/uz/muhandis/employeeservice/service/APIClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import uz.muhandis.employeeservice.dto.DepartmentDto;
 
 
-@FeignClient(name = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080, http://localhost:8082", value = "DEPARTMENT-SERVICE") //No Load balancing
+@FeignClient(name = "DEPARTMENT-SERVICE")          //With load balancing
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
