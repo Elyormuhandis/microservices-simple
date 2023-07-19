769f9e12de474f4038c4ca7cb01d92d9e476b609
elyormuhandis <elyormuhandis@gmail.com>
2023-07-19 07:51:26 +0500

    Add new module named Organization

---
+++ organization-service/src/main/java/uz/muhandis/organizationservice/OrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.muhandis.organizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServiceApplication.class, args);
+	}
+
+}
